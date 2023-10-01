16ae0565b6ab57b0188a16191b4f8a845d9d0888
Raul Rotilli Aguirre <raulrotilli1@gmail.com>
2023-10-01 19:58:25 -0300

    exercicio2 feito

--- aula1/src/com/exercicios/aula1/Exercicio2.java
+++ aula1/src/com/exercicios/aula1/Exercicio2.java
@@ -1,7 +1,20 @@
 package com.exercicios.aula1;
+
+import java.util.Scanner;
+
 //Crie um programa que receba 2 números e imprima na tela.
 public class Exercicio2 {
     public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
 
+        System.out.println("Digite o primeiro número:");
+
+         int n1 = Integer.parseInt(s.nextLine());
+
+        System.out.println("Digite o segundo número:");
+
+        int n2 = Integer.parseInt(s.nextLine());
+
+        System.out.println("O primeiro número digitado foi "+ n1 + " e o segundo número foi " + n2);
     }
 }
