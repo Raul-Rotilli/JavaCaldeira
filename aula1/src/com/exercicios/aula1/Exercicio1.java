fdfbf624f224697c3c5a708e0f49152dec414584
Raul Rotilli Aguirre <raulrotilli1@gmail.com>
2023-10-01 19:48:02 -0300

    exercicio1 feito

--- aula1/src/com/exercicios/aula1/Exercicio1.java
+++ aula1/src/com/exercicios/aula1/Exercicio1.java
@@ -1,7 +1,18 @@
 package com.exercicios.aula1;
+
+import java.util.Scanner;
+
 //Crie um programa que leia seu nome e imprima na tela.
 public class Exercicio1 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Qual o seu nome?");
+
+        String nome = s.nextLine();
+
+        System.out.println("Você registrou seu nome como " + nome);
+
 
     }
 }
