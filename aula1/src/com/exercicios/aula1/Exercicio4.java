f22a39692b29f2ea4c14f7ca33ee55d07584b23f
Raul Rotilli Aguirre <raulrotilli1@gmail.com>
2023-10-01 21:24:43 -0300

    exercicio4 feito

--- aula1/src/com/exercicios/aula1/Exercicio4.java
+++ aula1/src/com/exercicios/aula1/Exercicio4.java
@@ -3,8 +3,30 @@ package com.exercicios.aula1;
 // calcule a quantidade de salários mínimos que esse usuário ganha e imprima o resultado,
 // assuma que o salário mínimo é R$1.320.
 
+import java.math.MathContext;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Exercicio4 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#");
 
-    }
+        double salMinimo = 1320.00 ;
+
+        System.out.println("Digite o seu sálario: ");
+        double salUsuario = s.nextDouble();
+
+        double quantidadeSalariosMinimos = salUsuario / salMinimo;
+
+        if (salUsuario < salMinimo){
+            System.out.println("Seu salário de " + df.format(salUsuario) + " está abaixo do salário mínimo de " + salMinimo + " do Brasil na data de 01/10/2023.");
+        }else {
+            System.out.println("Seu salário de " + df.format(salUsuario) + " equivale a " + df.format(Math.floor(quantidadeSalariosMinimos)) + " salários mínimos no Brasil em 01/10/2023.");
+        }
+
+        }
+
+
 }
+
