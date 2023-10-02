5b2de00e700ccb4dc934f601a4d47237ee51132d
Raul Rotilli Aguirre <raulrotilli1@gmail.com>
2023-10-02 02:45:52 -0300

    exercicio6 feito

--- aula1/src/com/exercicios/aula1/Exercicio6.java
+++ aula1/src/com/exercicios/aula1/Exercicio6.java
@@ -1,4 +1,7 @@
 package com.exercicios.aula1;
+
+import java.util.Scanner;
+
 //Escreva um programa que leia a idade do usuário e imprima respostas diferentes dependendo do número colocado:
 //i. Se for menor de 16 anos, avisar que não pode votar;
 //ii. Se tiver 16 ou 17, avisar que o voto é facultativo;
@@ -6,6 +9,25 @@ package com.exercicios.aula1;
 //iv. De 18 até 65, é obrigatório votar.
 public class Exercicio6 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        System.out.println("Digite sua idade:");
+        int idade = s.nextInt();
+
+        if (idade <0){
+            System.out.println("Idade inválida!");
+        } else if (idade < 16) {
+            System.out.println("Você não pode votar ainda!");
+        } else if (idade <= 17) {
+            System.out.println("Seu voto é facultativo!");
+        } else if (idade <= 65) {
+            System.out.println("Seu voto é obrigatório!");
+        } else if (idade < 122) {
+            System.out.println("Seu voto é facultativo!");
+        }else {
+            System.out.println("Idade inválida");
+        }
     }
+        
 }
+
